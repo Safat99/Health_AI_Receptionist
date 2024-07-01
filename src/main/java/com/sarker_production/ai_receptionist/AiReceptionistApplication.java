7fdd53a255547b0e5ed1d1f6fc38d5bdb4bd03f9
Safat99 <sarkersafat99@gmail.com>
2024-07-01 10:26:20 -0600

    started project

---
+++ src/main/java/com/sarker_production/ai_receptionist/AiReceptionistApplication.java
@@ -0,0 +1,13 @@
+package com.sarker_production.ai_receptionist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiReceptionistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiReceptionistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sarker_production/ai_receptionist/AiReceptionistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarker_production.ai_receptionist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiReceptionistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
